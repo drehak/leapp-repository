c6d7685ab1d7aa38815d12ce45b31569e8ace59c
Zdenek Dohnal <zdohnal@redhat.com>
2019-05-20 15:10:55 +0200

    docstring for vim model

--- repos/system_upgrade/el7toel8/models/vimmodel.py
+++ repos/system_upgrade/el7toel8/models/vimmodel.py
@@ -3,5 +3,13 @@ from leapp.topics import SystemInfoTopic
 
 
 class VimModel(Model):
+    """
+    Class VimModel
+
+    Members:
+    topic
+    vim_configs - list of strings which are full paths to found vim configuration files.
+    """
+
     topic = SystemInfoTopic
     vim_configs = fields.List(fields.String())
