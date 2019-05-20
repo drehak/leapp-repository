93446de4128c35630567810b0e3ba060acf6e7fe
Zdenek Dohnal <zdohnal@redhat.com>
2019-05-20 15:54:06 +0200

    Changing indentation, substitute report_generic for log message, various changes.

--- repos/system_upgrade/el7toel8/actors/vimcheck/actor.py
+++ repos/system_upgrade/el7toel8/actors/vimcheck/actor.py
@@ -3,16 +3,6 @@ from leapp.libraries.common.reporting import report_generic
 from leapp.models import InstalledRedHatSignedRPM, VimModel, Report
 from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
 
-vimconfigs = {
-              'vim-minimal': '/etc/virc',
-              'vim-enhanced' : '/etc/vimrc'
-              }
-
-changes = [
-            'let skip_defaults_vim=1',
-            'set t_BE='
-          ]
-
 
 
 class VimCheck(Actor):
@@ -24,59 +14,59 @@ class VimCheck(Actor):
     consumes = (InstalledRedHatSignedRPM,)
     produces = (Report, VimModel)
     tags = (ChecksPhaseTag, IPUWorkflowTag)
+    vimconfigs = {
+                    'vim-minimal' : '/etc/virc',
+                    'vim-enhanced' : '/etc/vimrc'
+                 }
 
     def checkconfigs(self, packages_dict):
-      """
-      checkconfigs
+        """
+        checkconfigs
 
-      Vim project has two configs - /etc/virc for vi and /etc/vimrc for vim.
-      This method appends respective configuration filename into list according to
-      installed Vim packages.
+        Vim project has two configs - /etc/virc for vi and /etc/vimrc for vim.
+        This method appends respective configuration filename into list according to
+        installed Vim packages.
 
-      parameters:
-      - packages_dict - dictionary
+        parameters:
+        - packages_dict - dictionary
 
-      returns list of strings
-      """
-      configs = []
+        returns list of strings
+        """
+        configs = []
 
-      for package in packages_dict:
-        if packages_dict[package] == True:
-          configs.append(vimconfigs[package])
+        for package in packages_dict:
+            if packages_dict[package] is True:
+                configs.append(vimconfigs[package])
 
-      return configs
+        return configs
 
     def process(self):
-      vim_configs = []
-      installed_vim = {
-                        'vim-minimal' : False,
-                        'vim-enhanced' : False
-                      }
-
-      for rpms in self.consume(InstalledRedHatSignedRPM):
-        for pkg in rpms.items:
-          if pkg.name == 'vim-minimal':
-            installed_vim['vim-minimal'] = True
-          elif pkg.name == 'vim-enhanced':
-            installed_vim['vim-enhanced'] = True
-          else:
-            continue
-
-      vim_configs = self.checkconfigs(installed_vim)
-
-      if len(vim_configs) > 0:
-        report_generic(
-                        title='Vim configuration files will be migrated.',
-                        summary='Either virc or vimrc exists - it needs to be migrated.',
-                        severity='low'
-                      )
-      else:
-        report_generic(
-                        title='Vim configuration files do not exist.',
-                        summary='Vim is not installed or configuration files are removed.',
-                        severity='low'
-                      )
         vim_configs = []
+        installed_vim = {
+                          'vim-minimal' : False,
+                          'vim-enhanced' : False
+                        }
 
-      self.produce(VimModel(vim_configs=vim_configs))
+        installed_vim_pkgs = installed_vim.keys()
+        for rpms in self.consume(InstalledRedHatSignedRPM):
+            for pkg in rpms.items:
+                if pkg.name in installed_vim_pkgs:
+                    installed_vim[pkg.name] = True
+
+        vim_configs = self.checkconfigs(installed_vim)
+
+        if len(vim_configs) > 0:
+            report_generic(
+                            title='Vim configuration files will be migrated.',
+                            summary='Either /etc/virc (for vi) or /etc/vimrc (for vim and gvim) \
+                                     configuration files exist. Vim changed default configuration \
+                                     and default behavior of copy/paste, migration will put directives \
+                                     into configuration files for setting behavior as it was in Vim 7.4.',
+                            severity='low'
+                          )
+        else:
+            self.log.info('Vim configuration files do not exist. Vim is not installed or configuration files are removed.')
+            vim_configs = []
+
+        self.produce(VimModel(vim_configs=vim_configs))
 
--- repos/system_upgrade/el7toel8/actors/vimmigrate/actor.py
+++ repos/system_upgrade/el7toel8/actors/vimmigrate/actor.py
@@ -3,16 +3,6 @@ from leapp.libraries.common.reporting import report_generic
 from leapp.models import Report, VimModel
 from leapp.tags import ApplicationsPhaseTag, IPUWorkflowTag
 
-vimconfigs = {
-              'vim-minimal': '/etc/virc',
-              'vim-enhanced' : '/etc/vimrc'
-              }
-
-changes = [
-            'let skip_defaults_vim=1',
-            'set t_BE='
-          ]
-
 
 class VimMigrate(Actor):
     """
@@ -24,44 +14,45 @@ class VimMigrate(Actor):
     consumes = (VimModel,)
     produces = (Report,)
     tags = (ApplicationsPhaseTag, IPUWorkflowTag)
+    changes = [
+                'let skip_defaults_vim=1',
+                'set t_BE='
+              ]
+
 
     def insert_string(self, string, path):
-      """
-      insert_string
+        """
+        insert_string
 
-      Insert string into a file
+        Insert string into a file
 
-      parameters:
-      - string - string
-      - path - string
+        parameters:
+        - string - string
+        - path - string
 
-      returns boolean
-      """
+        returns boolean
+        """
 
-      try:
-        with open(path, 'a') as f:
-          f.write(string + '\n')
-          return True
-      except IOError:
-        return False
+        try:
+            with open(path, 'a') as f:
+                f.write(string + '\n')
+                return True
+        except IOError:
+            return False
 
     def process(self):
-      for configs_list in self.consume(VimModel):
-        if configs_list == []:
-          report_generic(
-                          title='Vim configuration files will not be migrated.',
-                          summary='No Vim configuration files exists.',
-                          severity='low'
-                        )
-        else:
-          for vim_config in configs_list.vim_configs:
-            for change in changes:
-              result = self.insert_string(change, vim_config)
-              if result is False:
-                raise StopActorExecutionError('Vimmigrate actor was not able to open configuration file {}'.format(vim_config))
+        for configs_list in self.consume(VimModel):
+            if configs_list == []:
+                self.log.info('Vim configuration files will not be migrated, no Vim configuration files exists.')
+            else:
+                for vim_config in configs_list.vim_configs:
+                    for change in changes:
+                        result = self.insert_string(change, vim_config)
+                        if result is False:
+                            raise StopActorExecutionError('Vimmigrate actor was not able to open configuration file {}'.format(vim_config))
 
-          report_generic(
-                          title='Vim was migrated.',
-                          summary='Changed strings were added into configuration files.',
-                          severity='low'
-                        )
+                report_generic(
+                                title='Vim was migrated.',
+                                summary='Changed strings were added into configuration files to ensure behavior from Vim 7.4.',
+                                severity='low'
+                              )
