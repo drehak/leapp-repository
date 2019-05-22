a2773e3ac185fcc4e76acd51f16557fa65a1eff1
Petr Stodulka <pstodulk@redhat.com>
2019-05-22 18:03:15 +0200

    Fix and modify the vim related actors

--- repos/system_upgrade/el7toel8/actors/vimmigrate/actor.py
+++ repos/system_upgrade/el7toel8/actors/vimmigrate/actor.py
@@ -1,59 +1,43 @@
+
 from leapp.actors import Actor
-from leapp.libraries.common.reporting import report_generic
-from leapp.models import Report, VimModel
+from leapp.libraries.actor import library
+from leapp.libraries.common.reporting import report_with_remediation
+from leapp.models import Report, InstalledRedHatSignedRPM
 from leapp.tags import ApplicationsPhaseTag, IPUWorkflowTag
 
-from os import access, W_OK
 
 class VimMigrate(Actor):
     """
-    Actor for adding two directives into vim configuration files to simulate 
-    the same behavior as vim-7.4 has.
+    Modify Vim configuration files to keep the the same behavior as vim-7.4 has.
     """
 
     name = 'vim_migrate'
-    consumes = (VimModel,)
+    consumes = (InstalledRedHatSignedRPM,)
     produces = (Report,)
     tags = (ApplicationsPhaseTag, IPUWorkflowTag)
 
-    """ Macros for adding into configuration file"""
-    _changes = [
-                'let skip_defaults_vim=1',
-                'set t_BE='
-              ]
-
-
-    def insert_string(self, string, path):
-        """
-        Insert string into a file
-
-        :param str string: string which will be inserted
-        :param str path: string representing path
-        """
-
-        if not access(path, W_OK):
-            raise OSError('File {} does not have writing permissions.'.format(path))
-
-        try:
-            with open(path, 'a') as f:
-                f.write(string + '\n')
-        except IOError:
-            raise IOError('Error during writing to file {}.'.format(path))
-
     def process(self):
-        for configs_list in self.consume(VimModel):
-            if not configs_list:
-                self.log.info('Vim configuration files will not be migrated, no Vim configuration files exists.')
-            else:
-                for vim_config in configs_list.vim_configs:
-                    for change in _changes:
-                        try:
-                            self._insert_string(change, vim_config)
-                        except (OSError, IOError) as error:
-                            raise StopActorExecutionError(error)
+        error_list = []
 
-                report_generic(
-                                title='Vim was migrated',
-                                summary='Changed strings were added into configuration files to ensure behavior from Vim 7.4',
-                                severity='low'
-                              )
+        # FIXME: use has_pkg from repo shared library once it is implemented
+        rh_rpms = [pkg.name for pkg in next(self.consume(InstalledRedHatSignedRPM), InstalledRedHatSignedRPM()).items]
+
+        for pkg, config_file in library.vim_configs.items():
+            if pkg not in rh_rpms:
+                continue
+            try:
+                library.update_config(config_file)
+            except (OSError, IOError) as error:
+                self.log.warning('Cannot modify the {} config file.'.format(config_file))
+                error_list.append((config_file, error))
+        if error_list:
+            report_with_remediation(
+                title='The Vim configuration has not been updated',
+                summary=('The files below has not been modified (error message included):'
+                         ''.join(['    - {}: {}'.format(err[0], err[1]) for err in error_list])),
+                remediation=('If you want keep original behaviour of vim, append the following lines'
+                             ' into those files:\n    {}'.format('    \n'.join(library.new_macros))),
+                severity='medium'
+            )
+
+            return
