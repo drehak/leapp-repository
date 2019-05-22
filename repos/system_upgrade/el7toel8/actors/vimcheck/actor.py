10251cf6a140e5708910a4f9099c5d4d8c79a50d
Zdenek Dohnal <zdohnal@redhat.com>
2019-05-22 15:57:55 +0200

    Changing docstring accoring PEP and guideline, various changes

--- repos/system_upgrade/el7toel8/actors/vimcheck/actor.py
+++ repos/system_upgrade/el7toel8/actors/vimcheck/actor.py
@@ -15,22 +15,21 @@ class VimCheck(Actor):
     produces = (Report, VimModel)
     tags = (ChecksPhaseTag, IPUWorkflowTag)
     vimconfigs = {
-                    'vim-minimal' : '/etc/virc',
-                    'vim-enhanced' : '/etc/vimrc'
+                    'vim-minimal': '/etc/virc',
+                    'vim-enhanced': '/etc/vimrc'
                  }
 
-    def checkconfigs(self, packages_dict):
+    def _checkconfigs(self, packages_dict):
         """
-        checkconfigs
-
         Vim project has two configs - /etc/virc for vi and /etc/vimrc for vim.
         This method appends respective configuration filename into list according to
         installed Vim packages.
 
-        parameters:
-        - packages_dict - dictionary
+        :param dict packages_dict: dictionary of name of Vim rpms plus boolean as key
+        if it is installed or not
 
-        returns list of strings
+        Returns list of strings, which represents full paths to vim configuration
+        files.
         """
         configs = []
 
@@ -41,27 +40,29 @@ class VimCheck(Actor):
         return configs
 
     def process(self):
+        """return value"""
         vim_configs = []
-        installed_vim = {
-                          'vim-minimal' : False,
-                          'vim-enhanced' : False
+        """ dictionary indicating which vim rpms are installed"""
+        _installed_vim = {
+                          'vim-minimal': False,
+                          'vim-enhanced': False
                         }
 
-        installed_vim_pkgs = installed_vim.keys()
+        installed_vim_pkgs = _installed_vim.keys()
         for rpms in self.consume(InstalledRedHatSignedRPM):
             for pkg in rpms.items:
                 if pkg.name in installed_vim_pkgs:
-                    installed_vim[pkg.name] = True
+                    _installed_vim[pkg.name] = True
 
-        vim_configs = self.checkconfigs(installed_vim)
+        vim_configs = self._checkconfigs(_installed_vim)
 
         if len(vim_configs) > 0:
             report_generic(
-                            title='Vim configuration files will be migrated.',
+                            title='Vim configuration files will be migrated',
                             summary='Either /etc/virc (for vi) or /etc/vimrc (for vim and gvim) \
                                      configuration files exist. Vim changed default configuration \
                                      and default behavior of copy/paste, migration will put directives \
-                                     into configuration files for setting behavior as it was in Vim 7.4.',
+                                     into configuration files for setting behavior as it was in Vim 7.4',
                             severity='low'
                           )
         else:
